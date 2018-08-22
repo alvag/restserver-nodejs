2dd8d70e26065d1bdb098580f4d59eab75de02f6
Max Alva <alva85@gmail.com>
2018-08-22 10:50:37 -0500

    Creando el archivo de configuracion global

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
+require('./config/config');
+
 const express = require( 'express' );
 const app = express();
+const bodyParser = require( 'body-parser' );
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hola Mundo');
 });
 
-app.listen(3000, () => {
-    console.log( 'Servidor corriendo en el puerto 3000.' );
+app.listen(process.env.PORT, () => {
+    console.log( `Servidor corriendo en el puerto ${process.env.PORT}.` );
 });
