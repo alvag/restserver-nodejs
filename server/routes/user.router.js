9b5a3d7b36610b3ec0f8e761740d49ff551ed654
Max Alva <alva85@gmail.com>
2018-08-22 12:05:13 -0500

    POST: creando un usuario en la coleccion

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -1,12 +1,37 @@
 const express = require( 'express' );
 const app = express();
+const User = require( '../models/user.model' );
 
 app.get('/usuario', (req, res) => {
     res.send('get Usuario');
 });
 
 app.post('/usuario', (req, res) => {
-    res.send('post Usuario');
+
+    let body = req.body;
+
+    let user = new User( {
+        name: body.name,
+        email: body.email,
+        password: body.password,
+        role: body.role
+    } );
+
+    user.save( ( error, userDB ) => {
+        if ( error ) {
+            return res.status( 400 ).json( {
+                ok: false,
+                error
+            } );
+        }
+
+        res.json( {
+            ok: true,
+            user: userDB
+        } );
+    } );
+
+
 });
 
 app.put('/usuario', (req, res) => {
