b2ab330b0f5511d192d9dd5589f438cce12acd86
Max Alva <alva85@gmail.com>
2018-08-22 11:39:42 -0500

    Mongoose

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('./config/config');
 
 const express = require( 'express' );
 const app = express();
+const mongoose = require( 'mongoose' );
 const bodyParser = require( 'body-parser' );
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -9,6 +10,12 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hola Mundo');
+});
+
+mongoose.connect('mongodb://localhost:27017/cafe', { useNewUrlParser: true }, (err, res) => {
+    if (err) throw err;
+
+    console.log( 'Conectado a la base de datos.' );
 });
 
 app.listen(process.env.PORT, () => {
