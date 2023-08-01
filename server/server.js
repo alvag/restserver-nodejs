abbdfda63d405b69bdd85f73f8bf51de305221d3
Max Alva <alva85@gmail.com>
2023-08-01 15:36:05 -0500

    fix deps

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.set('useFindAndModify', false);
 
 const bodyParser = require('body-parser');
 
@@ -16,14 +15,16 @@ app.use(express.static(path.resolve(__dirname, '../public')));
 app.use(require('./routes/index.router'));
 
 // eslint-disable-next-line no-unused-vars
-mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }, (error, res) => {
-    if (error) throw error;
+mongoose.connect(process.env.DB_URL).then(() =>{
     // eslint-disable-next-line no-console
-    console.log('Conectado a la base de datos.');
+    console.log('Conectado a la base de datos');
 
     app.listen(process.env.PORT, () => {
         // eslint-disable-next-line no-console
         console.log(`Servidor corriendo en el puerto ${process.env.PORT}.`);
     });
 
+}).catch((err) => {
+    // eslint-disable-next-line no-console
+    console.log('Error al conectar a la base de datos', err);
 });
